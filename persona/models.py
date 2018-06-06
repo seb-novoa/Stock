291a2314a99cd3feb15a32c43bc6201e9b6f193d
Sebastian <35838428+seb-novoa@users.noreply.github.com>
2018-06-06 09:23:34 -0400

    Change models Areas and Persona
    Add field CDC  to Areas
    add field MotherLastname to Persona

--- persona/models.py
+++ persona/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
 class Areas(models.Model):
+    CDC  = models.IntegerField(null = True)
     Area = models.CharField(max_length = 30)
+
 
 class Puestos(models.Model):
     Puesto = models.CharField(max_length = 30)
 
 class Personas(models.Model):
     Name  = models.CharField(max_length = 50)
-    Lastname = models.CharField(max_length = 50, null = True)
+    Lastname = models.CharField(max_length = 30, null = True)
+    MotherLastname = models.CharField(max_length = 30, null = True)
     Gestor  = models.ManyToManyField('self')
     Area    = models.ForeignKey(Areas, on_delete = models.CASCADE)
     Puesto  = models.ForeignKey(Puestos, on_delete = models. CASCADE)
@@ -20,4 +23,5 @@ class Personas(models.Model):
     def name_split(self):
         names = str.split(self.Name)
         self.Name = names[0]
-        self.Lastname = ''.join(names[1:])
+        self.Lastname = names[1]
+        self.MotherLastname = ''.join(names[2:])
--- persona/tests/test_views.py
+++ persona/tests/test_views.py
@@ -6,14 +6,15 @@ def initial_Areas_and_Puestos():
     ar = Areas.objects.create(Area = 'Area1')
     pu = Puestos.objects.create(Puesto = 'Puesto1')
     return ar, pu
-    
+
 class NewPersona(TestCase):
     def test_name_split_to_get_lastname(self):
         ar, pu = initial_Areas_and_Puestos()
         p1 = Personas.objects.create(Name = 'Nombre Apellido', Area = ar, Puesto = pu)
 
-        self.assertEqual(p1.Name, 'Nombre Apellido')
+        self.assertEqual(p1.Name, 'Nombre Apellido ApellidoMaterno')
 
         p1.name_split()
         self.assertEqual(p1.Name, 'Nombre')
         self.assertEqual(p1.Lastname, 'Apellido')
+        self.assertEqual(p1.MotherLastname, 'ApellidoMaterno')
